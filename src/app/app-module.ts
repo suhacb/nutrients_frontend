58f52dcab7b93bb90319209de662c89512d94102
Blaž Suhač <blazsuhac@mini.local>
2025-10-11 13:33:19 +0200

    Material 3 implemented.

--- src/app/app-module.ts
+++ src/app/app-module.ts
@@ -3,6 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing-module';
 import { App } from './app';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+
+/** Material 3 components and modules */
+
 
 @NgModule({
   declarations: [
@@ -10,7 +16,10 @@ import { App } from './app';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatButtonModule
   ],
   providers: [
     provideBrowserGlobalErrorListeners()
